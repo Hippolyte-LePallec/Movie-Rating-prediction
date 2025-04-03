4314eec6628748ca02fbb524ee030f95facd0b67
Albin condette <condettealbin@gmail.com>
2025-04-03 16:33:55 +0200

    card

--- pages/views/Film.php
+++ pages/views/Film.php
@@ -11,23 +11,22 @@
                         class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
                         <?php foreach ($films as $film): ?>
                             <div class="col">
-                                <div
-                                    class="card h-100 bg-dark text-light border-secondary shadow-lg rounded">
+                                <div class="card h-100 bg-dark text-light border-secondary shadow-lg rounded">
                                     <!-- Vérification de l'existence de l'image du film, sinon image par défaut -->
-                                    <img src="<?= !empty($film['image_url']) ? $film['image_url'] : 'https://m.media-amazon.com/images/M/MV5BYzZlMjE5ZTgtNDU4Yi00NWE0LWIzN2UtZDI5OTc3ZjRiYmYyXkEyXkFqcGc@._V1_SX300.jpg' ?>" class="card-img-top rounded fixed-size-image" alt="Film poster">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-warning"><?= htmlspecialchars($film['primaryTitle']) ?></h5>
+                                    <img src="<?= !empty($film['image_url']) ? $film['image_url'] : 'https://m.media-amazon.com/images/M/MV5BYzZlMjE5ZTgtNDU4Yi00NWE0LWIzN2UtZDI5OTc3ZjRiYmYyXkEyXkFqcGc@._V1_SX300.jpg' ?>" 
+                                        class="card-img-top rounded fixed-size-image" 
+                                        alt="Film poster">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                        <h5 class="card-title text-warning text-center"><?= htmlspecialchars($film['primaryTitle']) ?></h5>
                                         <p class="card-text small"><?= htmlspecialchars($film['startYear']) ?></p>
 
-                                        <p class="card-text">
+                                        <p class="card-text text-center">
                                             <strong>Genres:</strong>
                                             <span class="badge bg-secondary"><?= htmlspecialchars($film['genre_names']) ?></span>
                                         </p>
 
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <span class="badge bg-warning text-dark">★
-                                                <?= htmlspecialchars($film['averageRating'] ?? 'N/A') ?>
-                                            </span>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <span class="badge bg-warning text-dark">★ <?= htmlspecialchars($film['averageRating'] ?? 'N/A') ?></span>
                                         </div>
                                     </div>
                                 </div>
